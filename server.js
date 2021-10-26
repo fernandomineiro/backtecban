b5411ad2d014a9b161446eb13e3f15f30471f36f
fernando <fernandofitilan@hotmail.com>
2021-10-26 12:14:10 -0300

    up

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get("/", (req, res) => {
 
 require("./app/routes/physicalPerson.routes")(app);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
